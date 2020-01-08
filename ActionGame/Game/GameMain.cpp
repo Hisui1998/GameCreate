b7575316737ae8781ce6259bb4747c5f37d87203
HismiN <hisui4183@gmail.com>
2020-01-08 13:41:42 +0900

    こｃみ

--- ActionGame/Game/GameMain.cpp
+++ ActionGame/Game/GameMain.cpp
@@ -1,8 +1,6 @@
 #include "GameMain.h"
 #include "GameTitle.h"
 #include "Player.h"
-#include "Enemy.h"
-#include "ResourceMng.h"
 
 GameMain::GameMain()
 {
@@ -10,38 +8,35 @@ GameMain::GameMain()
 	else std::cout << "ƒQ[ƒ€ƒV[ƒ“‰Šú‰»Ž¸”s" << std::endl;
 }
 
+
 GameMain::~GameMain()
 {
 }
 
 bool GameMain::Init()
 {
-	_bgImage.emplace_back(IMAGE_ID("../Resource/BackGround/”wŒi¯‚Ì‚Ý.png"));
-	_bgImage.emplace_back(IMAGE_ID("../Resource/BackGround/”wŒiŽR‚Ì‚Ý.png"));
-	_bgImage.emplace_back(IMAGE_ID("../Resource/BackGround/”wŒiŒŽ‚Ì‚Ý.png"));
+	_bgImage.emplace_back(LoadGraph("../Resource/BackGround/”wŒi¯‚Ì‚Ý.png"));
+	_bgImage.emplace_back(LoadGraph("../Resource/BackGround/”wŒiŽR‚Ì‚Ý.png"));
+	_bgImage.emplace_back(LoadGraph("../Resource/BackGround/”wŒiŒŽ‚Ì‚Ý.png"));
 	_player = std::make_unique<Player>();
-	_enemy = std::make_unique<Enemy>(Vec2(0, 100), Vec2(30, 30));
+	lpMapCtl->RoadMap("mapdata-1");
 	return true;
 }
 
-std::unique_ptr<Scene> GameMain::Update(std::unique_ptr<Scene>& _this, char key[256])
+std::unique_ptr<Scene> GameMain::Update(std::unique_ptr<Scene> &_this, char key[256])
 {
 	_player->UpDate(key);
-	_enemy->UpDate(key);
+	_player->Draw();
+
 	return std::move(_this);
 }
 
 void GameMain::Draw()
 {
 	_drawFlame++;
-	for (auto& bg : _bgImage)
+	for (auto& bg:_bgImage)
 	{
-		for (auto& image : bg)
-		{
-			DrawExtendGraph(0, 0, 1440, 600, image, true);
-		}
+		DrawExtendGraph(0, 0, 1440,600,bg,true);
 	}
-	_player->Draw();
-	_enemy->Draw();
 	DrawString(0, 0, "MainScene", 0xffffff);
 }
