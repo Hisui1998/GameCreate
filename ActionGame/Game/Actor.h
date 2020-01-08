1a4023386941b4e8cb57812ae85e71e059f7432c
HismiN <hisui4183@gmail.com>
2020-01-08 13:38:59 +0900

    こみと

--- ActionGame/Game/Actor.h
+++ ActionGame/Game/Actor.h
@@ -12,11 +12,23 @@ struct Vec2 {
 
 class Actor
 {
+protected:
+	Vec2 pos;		//位置情報
+	Vec2 dir;		//向き情報
+	float speed;	//速度
+	Vec2 size;		//アクターの画像サイズ
+
+	bool alive;		//生存フラグ
 
 public:
 	virtual ~Actor() {};
 	virtual bool Init() = 0;
 	virtual void UpDate(char* key) = 0;
 	virtual void Draw() = 0;
+
+	//Get関数
+	Vec2 GetPos() { return pos; };
+	Vec2 GetDir() { return dir; };
+	Vec2 GetSize() { return size; };
 };
 
